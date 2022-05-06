7cefdb9391eb8b2e6d69fe0170a175983c070204
TapiwaJoel Mudavanhu <tapiwajoel@gmail.com>
2022-05-06 09:41:10 +0200

    project init

---
+++ src/main/java/com/sovtech/graphql/spring/swapiapi/GraphqlSpringSwapiApiApplication.java
@@ -0,0 +1,13 @@
+package com.sovtech.graphql.spring.swapiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringSwapiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringSwapiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sovtech/graphql/spring/swapiapi/GraphqlSpringSwapiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovtech.graphql.spring.swapiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringSwapiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
